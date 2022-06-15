750a311de5caa7c3d802892c4337698014788449
martin <martin@>
2022-06-15 11:40:29 +0300

    Update terraform code to support latest terraform version (1.2.2):  * Update code and syntax to 0.13 version  * Remove remote backend  * Use newer azurerm provider  * Stop using obsolete service principal module, use azureread provider instead  * Define .terraform-version for tfenv

--- container-registry/backend.tf
+++ container-registry/backend.tf
@@ -1,8 +1,28 @@
 terraform {
-  backend "azurerm" {
+  /*   backend "azurerm" {
     resource_group_name   = "dev-storages"
     storage_account_name  = "tfstatesfordev"
     container_name        = "tfstates"
     key                   = "acr-dev.terraform.tfstate"
+  } */
+  required_providers {
+    azurerm = {
+      version = "~> 2.99.0"
+      source  = "hashicorp/azurerm"
+    }
+    azuread = {
+      version = "2.23.0"
+      source  = "hashicorp/azuread"
+    }
   }
+}
+
+provider "azurerm" {
+
+  features {
+  }
+}
+
+provider "azuread" {
+  tenant_id = var.tenant_id
 }
--- container-registry/outputs.tf
+++ container-registry/outputs.tf
@@ -1,11 +1,13 @@
 output "registry_endpoint" {
-  value = "${azurerm_container_registry.acr.login_server}"
+  value = azurerm_container_registry.acr.login_server
 }
 
 output "admin_username" {
-  value = "${azurerm_container_registry.acr.admin_username}"
+  value = azurerm_container_registry.acr.admin_username
+  sensitive = true
 }
 
 output "admin_password" {
-  value = "${azurerm_container_registry.acr.admin_password}"
+  value = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
