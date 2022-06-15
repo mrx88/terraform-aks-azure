3fd86e5e0e53482c7773a8374dfb52eb16962bef
martin <martin@>
2022-06-15 11:43:58 +0300

    Update terraform code to support latest terraform version (1.2.2):
     * Update code and syntax to 0.13 version
     * Remove remote backend
     * Use newer azurerm provider
     * Stop using obsolete service principal module, use azureread provider instead
     * Define .terraform-version for tfenv

--- aks-cluster/main.tf
+++ aks-cluster/main.tf
@@ -2,33 +2,31 @@
 # Based on https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html
 resource "azurerm_resource_group" "kube" {
   name     = "${var.prefix}-k8s-resources"
-  location = "${var.location}"
+  location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "kube" {
   name                = "${var.prefix}-k8s"
-  location            = "${azurerm_resource_group.kube.location}"
-  resource_group_name = "${azurerm_resource_group.kube.name}"
+  location            = azurerm_resource_group.kube.location
+  resource_group_name = azurerm_resource_group.kube.name
   dns_prefix          = "${var.prefix}-k8s"
 
-  agent_pool_profile {
+  default_node_pool {
+    node_count = 1
     name            = "default"
-    count           = 1
-    vm_size         = "Standard_B2s"
-    os_type         = "Linux"
-    os_disk_size_gb = 30
+    vm_size         = var.sku
   }
 
   service_principal {
-    client_id     = "${var.kubernetes_client_id}"
-    client_secret = "${var.kubernetes_client_secret}"
+    client_id     = var.kubernetes_client_id
+    client_secret = var.kubernetes_client_secret
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment
   }
 
   depends_on = [
-    "azurerm_resource_group.kube"
+    azurerm_resource_group.kube
   ]
 }
