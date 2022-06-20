35cf93f21a9b359b2cec2cb9ae1b7718f34ae039
martin <martin@>
2022-06-20 15:15:22 +0300

    * Add separate k8s node pool for app layer * Add separate subnets (for k8s node pools)

--- aks-cluster/main.tf
+++ aks-cluster/main.tf
@@ -1,5 +1,6 @@
 # AKS cluster creation
-# Based on https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html
+# Documentation https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster
+
 resource "azurerm_resource_group" "kube" {
   name     = "${var.prefix}-k8s-resources"
   location = var.location
@@ -13,25 +14,33 @@ resource "azurerm_virtual_network" "appvnet" {
 }
 
 resource "azurerm_subnet" "appsubnet" {
-  name                 = azurerm_virtual_network.appvnet.name
+  name                 = lookup(element(var.subnet_prefix, count.index), "name")
+  count                = length(var.subnet_prefix)
   resource_group_name  = azurerm_resource_group.kube.name
   virtual_network_name = azurerm_virtual_network.appvnet.name
-  address_prefixes     = var.vnetsubnet
+  address_prefixes     = lookup(element(var.subnet_prefix, count.index), "ip")
 }
 
 resource "azurerm_kubernetes_cluster" "kube" {
-  name                = "${var.prefix}-k8s"
-  location            = azurerm_resource_group.kube.location
-  resource_group_name = azurerm_resource_group.kube.name
-  dns_prefix          = "${var.prefix}-k8s"
+  name                            = "${var.prefix}-k8s"
+  location                        = azurerm_resource_group.kube.location
+  resource_group_name             = azurerm_resource_group.kube.name
+  dns_prefix                      = "${var.prefix}-k8s"
+  kubernetes_version              = var.kubernetes_version
+  api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
 
   default_node_pool {
     node_count     = var.node_count
     name           = "default"
     vm_size        = var.sku
-    vnet_subnet_id = azurerm_subnet.appsubnet.id
+    vnet_subnet_id = azurerm_subnet.appsubnet[0].id
   }
 
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+
+  }
   service_principal {
     client_id     = var.kubernetes_client_id
     client_secret = var.kubernetes_client_secret
@@ -43,6 +52,28 @@ resource "azurerm_kubernetes_cluster" "kube" {
 
   depends_on = [
     azurerm_resource_group.kube,
+    azurerm_virtual_network.appvnet
+  ]
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "name" {
+  name                  = "userapplayer"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.kube.id
+  node_count            = var.node_count
+  vm_size               = var.sku
+  vnet_subnet_id        = azurerm_subnet.appsubnet[1].id
+  max_pods              = var.max_pods
+  mode                  = "User"
+  enable_auto_scaling   = true
+  min_count             = var.min_count
+  max_count             = var.max_count
+
+  tags = {
+    Environment = var.environment
+  }
+
+  depends_on = [
+    azurerm_kubernetes_cluster.kube,
     azurerm_virtual_network.appvnet
   ]
 }
--- aks-cluster/variables.tf
+++ aks-cluster/variables.tf
@@ -30,12 +30,46 @@ variable "vnetcidr" {
   default     = ["192.168.0.0/22"]
 }
 
-variable "vnetsubnet" {
-  description = "The subnet(s) of the VNet to use for the AKS cluster"
-  default     = ["192.168.0.0/24"]
+variable "subnet_prefix" {
+  type = list(any)
+  default = [
+    {
+      ip   = ["192.168.0.0/24"]
+      name = "subnet-1-system"
+    },
+    {
+      ip   = ["192.168.1.0/24"]
+      name = "subnet-2-apps"
+    }
+  ]
 }
 
 variable "node_count" {
   description = "The number of nodes to use for the AKS cluster"
   default     = 1
+}
+
+variable "max_pods" {
+  description = "The maximum number of pods that can be run on a node"
+  default     = 250
+}
+
+variable "min_count" {
+  description = "The minimum number of nodes to use for the AKS cluster (autoscaling)"
+  default     = 1
+}
+
+variable "max_count" {
+  description = "The maximum number of nodes to use for the AKS cluster (autoscaling)"
+  default     = 2
+}
+
+variable "kubernetes_version" {
+  description = "The version of Kubernetes to use for the AKS cluster"
+  default     = "1.23.5"
+}
+
+variable "api_server_authorized_ip_ranges" {
+  description = "The IP ranges that can access the K8s API server"
+  default     = ["0.0.0.0/32"]
 }
